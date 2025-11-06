5994c72f104c5a3aa5f33eb968f1b41039adfcac
abbhagat <abhinaw.kumar7@gmail.com>
2025-11-06 10:30:58 +0530

    Learning Kafka

--- kafka-streams-api/src/main/java/com/kafka/streams/api/topology/AggregateTopology.java
+++ kafka-streams-api/src/main/java/com/kafka/streams/api/topology/AggregateTopology.java
@@ -1,10 +1,12 @@
 package com.kafka.streams.api.topology;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.Topology;
 import org.apache.kafka.streams.kstream.*;
 
+@Slf4j
 public class AggregateTopology {
 
   private static final StreamsBuilder streamsBuilder = new StreamsBuilder();
@@ -14,7 +16,15 @@ public class AggregateTopology {
     aggregateStreams.print(Printed.<String, String>toSysOut().withLabel("aggregate"));
     KGroupedStream<String, String> groupedStream = aggregateStreams.groupByKey(Grouped.with(Serdes.String(), Serdes.String()));
     exploreCount(groupedStream);
+    exploreReduce(groupedStream);
     return streamsBuilder.build();
+  }
+
+  private static void exploreReduce(KGroupedStream<String, String> groupedStream) {
+    groupedStream.reduce((value1, value2) -> {
+      log.info("value1 :{}- value2 :{}", value1, value2);
+      return value1.toUpperCase() + value2.toUpperCase();
+    });
   }
 
   private static void exploreCount(KGroupedStream<String, String> groupedStream) {
